5e282605f8477117a4b6384c768e2a97359e2d0a
floralunit <anya71034@gmail.com>
2023-08-29 22:02:44 +0300

    программа для создания массива файлов для реакта из папки

---
+++ FilesToJsArray/FilesToJsArray/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace FilesToJsArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string path = args[0]; // получаем путь к папке из аргументов командной строки
+            Console.WriteLine("Enter the folder path: ");
+            string path = Console.ReadLine();
+            
+            string[] extensions = { ".png", ".jpg", ".jpeg", ".gif", ".svg" }; // список расширений файлов, которые нужно добавить в массив
+
+            List<string> files = new List<string>();
+            foreach (string extension in extensions)
+            {
+                files.AddRange(Directory.GetFiles(path, "*" + extension));
+            }
+
+            List<object> photos = new List<object>();
+            foreach (string file in files)
+            {
+                photos.Add(new { src = string.Format("require(" + "'./" + Path.GetFileName(file) + "'" + ")") });
+            }
+            var serializer = new JsonSerializer();
+            var stringWriter = new StringWriter();
+            using (var writer = new JsonTextWriter(stringWriter))
+            {
+                writer.QuoteName = false;
+                serializer.Serialize(writer, photos);
+            }
+            var json = stringWriter.ToString();
+            string output = $"export const {Path.GetFileName(path).Replace("-", "_")} = " + json + ";"; // сериализуем массив в формат JSON и записываем его в строку
+
+            string outputPath = Path.Combine(path, Path.GetFileName(path) + ".js"); // создаем путь для выходного файла
+            File.WriteAllText(outputPath, output.Replace("\"", "")); // записываем данные в выходной файл
+        }
+    }
+}
